42db851df16172fada9a15326acc0a61b6cab38d
asnabryg <8mixu8@gmail.com>
2021-08-27 15:57:04 +0300

    5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "../index.css"
 
-const Blog = ({ blog }) => (
-    <div>
-        {blog.title} {blog.author}
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false)
+
+  return (
+    <div className="blogStyle">
+      {blog.title} {blog.author}
+      <button onClick={() => setView(!view)}>
+        {view ? "hide": "view"}
+      </button>
+      {view
+        ?
+        <>
+          <br/>
+          {blog.url} <br />
+          {blog.likes}
+          <button>like</button> <br/>
+          {blog.user.username}
+        </>
+        :
+        <></>
+      }
     </div>
-)
+  )
+}
 
 export default Blog
