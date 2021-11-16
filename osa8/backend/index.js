5149f135b9788c8ba1bf53eb11482fdde573f5d0
asnabryg <8mixu8@gmail.com>
2021-11-16 16:15:41 +0200

    korjaus

--- osa8/backend/index.js
+++ osa8/backend/index.js
@@ -1,10 +1,12 @@
 const { ApolloServer, gql } = require("apollo-server")
-const { v1: uuid } = require("uuid")
+require("dotenv").config()
 const mongoose = require("mongoose")
 const Book = require("./models/Book")
 const Author = require("./models/Author")
+const jwt = require("jsonwebtoken")
 
-const MONGODB_URI = "mongodb+srv://mixu:kukka@cluster0.3adn9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const MONGODB_URI = process.env.MONGODB_URL
+const JWT_SECRET = process.env.SECRET
 
 console.log('connecting to', MONGODB_URI)
 
@@ -15,84 +17,6 @@ mongoose.connect(MONGODB_URI)
   .catch((error) => {
     console.log('error connection to MongoDB:', error.message)
   })
-
-// let authors = [
-//   {
-//     name: 'Robert Martin',
-//     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-//     born: 1952,
-//   },
-//   {
-//     name: 'Martin Fowler',
-//     id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-//     born: 1963
-//   },
-//   {
-//     name: 'Fyodor Dostoevsky',
-//     id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-//     born: 1821
-//   },
-//   {
-//     name: 'Joshua Kerievsky', // birthyear not known
-//     id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-//   },
-//   {
-//     name: 'Sandi Metz', // birthyear not known
-//     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-//   },
-// ]
-
-// let books = [
-//   {
-//     title: 'Clean Code',
-//     published: 2008,
-//     author: 'Robert Martin',
-//     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring']
-//   },
-//   {
-//     title: 'Agile software development',
-//     published: 2002,
-//     author: 'Robert Martin',
-//     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-//     genres: ['agile', 'patterns', 'design']
-//   },
-//   {
-//     title: 'Refactoring, edition 2',
-//     published: 2018,
-//     author: 'Martin Fowler',
-//     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring']
-//   },
-//   {
-//     title: 'Refactoring to patterns',
-//     published: 2008,
-//     author: 'Joshua Kerievsky',
-//     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring', 'patterns']
-//   },
-//   {
-//     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-//     published: 2012,
-//     author: 'Sandi Metz',
-//     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring', 'design']
-//   },
-//   {
-//     title: 'Crime and punishment',
-//     published: 1866,
-//     author: 'Fyodor Dostoevsky',
-//     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-//     genres: ['classic', 'crime']
-//   },
-//   {
-//     title: 'The Demon ',
-//     published: 1872,
-//     author: 'Fyodor Dostoevsky',
-//     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-//     genres: ['classic', 'revolution']
-//   },
-// ]
 
 const typeDefs = gql`
   type Book {
@@ -108,12 +32,23 @@ const typeDefs = gql`
     born: Int
     bookCount: Int
   }
+
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
   
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+    me: User
   }
 
   type Mutation {
@@ -128,6 +63,16 @@ const typeDefs = gql`
       name: String!
       setBornTo: Int!
     ): Author
+
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 `
 
@@ -165,7 +110,7 @@ const resolvers = {
           await author.save()
         } catch (error) {
           throw new UserInputError(error.message, {
-            invalidArgs: args,
+            invalidArgs: args
           })
         }
       }
@@ -186,7 +131,7 @@ const resolvers = {
     },
 
     editAuthor: async (root, args) => {
-      let author = await Author.findOne({name: args.name})
+      let author = await Author.findOne({ name: args.name })
       author.born = args.setBornTo
       return await author.save()
     }
